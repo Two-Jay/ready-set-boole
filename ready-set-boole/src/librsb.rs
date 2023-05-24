5c4354e7149210e57e7d9072d793b2b196c041c3
Two-Jay <djeeee1272@gmail.com>
2023-05-24 17:29:53 +0900

    edit multipier O(n) to O(1)

--- ready-set-boole/src/librsb.rs
+++ ready-set-boole/src/librsb.rs
@@ -13,7 +13,7 @@ pub fn multiplier(left: u32, right:u32) -> u32 {
     let mut tmp_left = left;
     let mut tmp_right = right;
     let mut result = 0;
-    while tmp_right > 0 {
+    for _ in 0..32 {
         if tmp_right & 1 != 0 {
             result = adder(result, tmp_left);
         }
