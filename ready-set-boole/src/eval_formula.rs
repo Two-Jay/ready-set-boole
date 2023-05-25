5bbd1bee89056a3e5fa2d97bb67f43d269e29be3
Two-Jay <djeeee1272@gmail.com>
2023-05-25 18:12:13 +0900

    update

--- ready-set-boole/src/eval_formula.rs
+++ ready-set-boole/src/eval_formula.rs
@@ -1,3 +1,25 @@
+use std::collections::VecDeque;
+use std::collections::Vec;
+
+fn get_F(&char ch, &VecDeque vd) -> i8 {
+    vd.push_back(0);
+}
+
+fn get_T(&char ch, &VecDeque vd) -> i8 {
+    vd.push_back(1);
+}
+
+fn init_state_behavior(&Vec behaviors) {
+    behaviors[0] = get_F;
+    behaviors[1] = get_T;
+}
+
 pub fn eval_formula(_formula : &str) -> bool {
+    let mut vecdeque = new VecDeque();
+    let mut state_behaviors = new Vec();
+
+    for _ in str {
+        
+    }
     return true;
 }
