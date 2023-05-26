6fda2d4468d0e95feab689cf4401df989c326fda
Two-Jay <djeeee1272@gmail.com>
2023-05-26 19:34:20 +0900

    negation complete

--- ready-set-boole/src/eval_formula.rs
+++ ready-set-boole/src/eval_formula.rs
@@ -10,6 +10,19 @@ fn get_true(vd : &mut VecDeque<i8>) -> Option<f64> {
     None
 }
 
+fn condition_get_true(value : &char, negation_flag : &bool) -> bool{
+    return *value == '1' || (*value == '0' && *negation_flag == true);
+}
+
+fn condition_get_false(value : &char, negation_flag : &bool) -> bool{
+    return *value == '0' || (*value == '1' && *negation_flag == true);
+}
+
+fn switch_negation_flag(flag : &mut bool) -> Option<f64> {
+    *flag = if *flag == true  { false } else { true };
+    None
+}
+
 fn init_state_behavior(behaviors : &mut Vec<fn(&mut VecDeque<i8>) -> Option<f64>>) {
     behaviors.push(get_false);
     behaviors.push(get_true);
@@ -18,15 +31,17 @@ fn init_state_behavior(behaviors : &mut Vec<fn(&mut VecDeque<i8>) -> Option<f64>
 pub fn eval_formula(_formula : &str) -> bool {
     let mut stored : VecDeque<i8> = VecDeque::new();
     let mut state_behaviors : Vec<fn(&mut VecDeque<i8>) -> Option<f64>> = Vec::new();
+    let mut _negation_flag : bool = false;
 
     init_state_behavior(&mut state_behaviors);
     let values = _formula.chars();
     for value in values {
-        if value == '0' {
+        if condition_get_false(&value, &_negation_flag) {
             state_behaviors[0](&mut stored);
-        }
-        if value == '1' {
+        } else if condition_get_true(&value, &_negation_flag) {
             state_behaviors[1](&mut stored);
+        } else if value == '!' {
+            switch_negation_flag(&mut _negation_flag);
         }
     }
     return true;
